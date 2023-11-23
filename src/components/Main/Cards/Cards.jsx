82db1ed228bc062f14cfeca76acda2291fffb4e6
Nora Westphal <tupfen@hotmail.de>
2023-11-23 17:29:22 +0100

    cards are fetched from localhost:8080

--- src/components/Main/Cards/Cards.jsx
+++ src/components/Main/Cards/Cards.jsx
@@ -8,25 +8,30 @@ import CardPages from './CardPages';
 export default function Cards() {
 
     const [countryArray, setCountryArray] = useState([])
-    const {VITE_CF_TOKEN,VITE_SPACE_ID} = import.meta.env;
 
-    const url = `https://cdn.contentful.com/spaces/${VITE_SPACE_ID}/entries?access_token=${VITE_CF_TOKEN}&content_type=countryCard`
-
+    // Deprecated use of Contentful
+    //const {VITE_CF_TOKEN,VITE_SPACE_ID} = import.meta.env;
+    //const url = `https://cdn.contentful.com/spaces/${VITE_SPACE_ID}/entries?access_token=${VITE_CF_TOKEN}&content_type=countryCard`
+    
+    const url2 =  `http://localhost:8080/cards`
+    
     function handleData(data) {
-        setCountryArray(data.items)
+        console.log(data[0])
+        
+        setCountryArray(data)
     }
 
     useEffect(() => {
-        fetchData(url, handleData)
+        fetchData(url2, handleData)
     }, []);
 
     function createCards () {
         return countryArray?.map((country) => <Card
-                    key={country?.fields.name}
-                    country={country?.fields.name}
-                    imgId={country?.fields.image.sys.id}
-                    text={country?.fields.teaserText}
-                    detailsId={country?.fields.details.sys.id} />)
+                    key={country?.name}
+                    country={country?.name}
+                    imgId={country?.imagepath_id}
+                    text={country?.description}
+                    detailsId={country?.countrydetails_id} />)
     }
 
 
